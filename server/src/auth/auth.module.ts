d0458d9597e61da55b83ffba63f71c8c8a5cfb04
Jungma1 <mbjun3630@naver.com>
2021-12-15 20:18:07 +0900

    Add oauth2 callback

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { GoogleStrategy } from './strategy/google.strategy';
 
-@Module({})
+@Module({
+  controllers: [AuthController],
+  providers: [GoogleStrategy],
+})
 export class AuthModule {}
