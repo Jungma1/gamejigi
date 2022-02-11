4fb58c2ffa51a40a236d2b338ad6d731ba729bf3
Jungma1 <mbjun3630@naver.com>
2022-02-11 13:29:24 +0900

    feat: delete fastify

--- server-fastify/src/routes/api/auth/index.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyPluginCallback } from 'fastify';
-import socialRoute from './social';
-
-const authRoute: FastifyPluginCallback = (fastify, opts, done) => {
-  fastify.register(socialRoute, { prefix: '/social' });
-  done();
-};
-
-export default authRoute;
--- server-fastify/src/routes/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyPluginCallback } from 'fastify';
-import authRoute from './auth';
-
-const apiRoutes: FastifyPluginCallback = (fastify, opts, done) => {
-  fastify.register(authRoute, { prefix: '/auth' });
-  done();
-};
-
-export default apiRoutes;
